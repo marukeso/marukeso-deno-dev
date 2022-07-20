0843ddf0b25cf03f66c0156b7d2b887af1848ba0
Kentaro Maruyama <marukeso@gmail.com>
2022-07-20 16:28:24 +0900

    feat: add favicon and social icons

--- main.ts
+++ main.ts
@@ -6,17 +6,20 @@ blog({
   avatar: "https://avatars.githubusercontent.com/u/5692384?v=4",
   avatarClass: "rounded-full",
   author: "marukeso",
-
+  links: [
+    { title: "Email", url: "mailto:marukeso@gmail.com" },
+    { title: "GitHub", url: "https://github.com/marukeso" },
+  ],
+  lang: "ja",
+  // dateStyle: "long", // localised format based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
   // middlewares: [
-
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+  //   ga("UA-XXXXXXXX-X"),
+  //   redirects({
+  //     "/foo": "/my_post",
+  //     // you can skip leading slashes too
+  //     "bar": "my_post2",
+  //   }),
+  // ],
+  // unocss: unocss_opts, // check https://github.com/unocss/unocss
+  favicon: "favicon.ico",
 });
